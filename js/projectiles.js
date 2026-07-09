ca0cbf49363d1196f87f074dcd559f30096dc170
notpikachuarmy <notpikachuarmy@gmail.com>
2026-07-09 17:13:34 +0200

    Delete js directory

--- js/projectiles.js
+++
@@ -1,6 +0,0 @@
-function markProjectileFaction(projectile, faction) {
-  if (!projectile) return projectile;
-  projectile.faction = faction;
-  projectile.sourcePrefix = faction;
-  return projectile;
-}
