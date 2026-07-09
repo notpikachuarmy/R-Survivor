ed458deaf9bd94e432b524514172cb887f6c6534
notpikachuarmy <notpikachuarmy@gmail.com>
2026-07-09 16:43:52 +0200

    Delete js directory

--- js/allies.js
+++
@@ -1,7 +0,0 @@
-function markAsAlly(entity) {
-  if (!entity) return entity;
-  entity.faction = "ally";
-  entity.isAlly = true;
-  entity.tags = [...new Set([...(entity.tags || []), "ally"] )];
-  return entity;
-}
